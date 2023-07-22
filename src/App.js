048bf7cda2efb26c4c14acc8a94ff3e091b11654
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2023-07-22 08:50:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ const App = () => {
 
   const handleGetSentiment = async () => {
     setLoader(true);
+    setError(null);
     setSentiment(null);
     const apiKey = "HP9r7jHtOMuqg4pe7NY1Ig==2V1iCTCutkI4ZZeN";
     const apiUrl = `https://api.api-ninjas.com/v1/sentiment?text=${encodeURIComponent(input)}`;
